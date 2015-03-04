a2993cb718efee5b0cc1f2e31ae851fccf9f75b5
Jerel Navarrete <navarrete.jerel@gmail.com>
2015-03-03 20:37:31 -0800

    resize image on upload

--- app/controllers/give_away_items_controller.rb
+++ app/controllers/give_away_items_controller.rb
@@ -20,6 +20,12 @@ class GiveAwayItemsController < ApplicationController
     @item = GiveAwayItem.find(params[:id])
   end
 
+  def destroy
+    @item = GiveAwayItem.find(params[:id])
+    @item.destroy
+    redirect_to give_away_items_path, notice: 'Item was sucessfully destroyed'
+  end
+
   private
   def item_params
     params.require(:give_away_item).permit(:title, :description, :give_away_item_image)
