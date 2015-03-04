41bdbbe89ba3fb657c65887eba47acfb797c0280
Jerel Navarrete <navarrete.jerel@gmail.com>
2015-03-03 21:06:08 -0800

    fixed resizing on upload

--- app/uploaders/give_away_item_image_uploader.rb
+++ app/uploaders/give_away_item_image_uploader.rb
@@ -25,7 +25,7 @@ class GiveAwayItemImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [200, 200]
+  process resize_to_fit: [200, 200]
   #
   # def scale(width, height)
   #   # do something
